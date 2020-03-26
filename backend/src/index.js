83e65958cc0f33750f648c95a1da8131b8341c3a
Carlos Doki <carlosdoki@gmail.com>
2020-03-26 00:18:39 -0300

    Inclusao Agente AppDynamics

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
 
+require("appdynamics").profile({
+    controllerHostName: '169.48.106.35',
+    controllerPort: 8090, 
+    controllerSslEnabled: false,  // Set to true if controllerPort is SSL
+    accountName: 'customer1',
+    accountAccessKey: '684c8100-1059-4000-ab68-60b7eea671a8', //required
+    applicationName: 'OmniStack11',
+    tierName: 'Backend', 
+    nodeName: 'Node' 
+   });
 const app = express();
 
 app.use(express.json());
