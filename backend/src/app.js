e9b691fd35616b648df996a2f4f7643034606a80
Carlos Doki <carlosdoki@gmail.com>
2020-03-29 13:06:45 -0300

    Include Tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 require("appdynamics").profile({
@@ -17,5 +18,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
