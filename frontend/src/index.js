8461cb0d7ecd8f24a778677a66339e4c3c30c31c
Carlos Doki <carlosdoki@gmail.com>
2020-03-29 17:49:08 -0300

    AppDynamics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+var appd = require('appdynamics');
+
+appd.profile({
+  controllerHostName: '169.48.106.35',
+  controllerPort: 8090, 
+  controllerSslEnabled: false,  // Set to true if controllerPort is SSL
+  accountName: 'customer1',
+  accountAccessKey: '684c8100-1059-4000-ab68-60b7eea671a8', //required
+  applicationName: 'OmniStack11',
+  tierName: 'Front-end', 
+  nodeName: 'Node1' 
+ });
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
